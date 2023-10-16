90af88531dd2e34619bcb2aae86e050dbf085e2b
Alejandro Dumas <alejandrodumasportfolio@gmail.com>
2023-10-16 14:30:43 -0700

    Update settings

--- intraweb/settings.py
+++ intraweb/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
 # Default primary key field type
